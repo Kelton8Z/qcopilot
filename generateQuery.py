0c976de2ccc11b90deb19e3d80988fb0fc67e716
Kelton8Z <zhuoran2@andrew.cmu.edu>
2024-06-11 10:29:09 +0800

    query gen

---
+++ generateQuery.py
@@ -0,0 +1,19 @@
+from llama_index.core.evaluation import generate_question_context_pairs
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core.node_parser import SentenceSplitter
+
+llm = OpenAI(model="gpt-4o")
+from llama_index.llms.openai import OpenAI 
+
+documents = SimpleDirectoryReader("./data").load_data()
+node_parser = SentenceSplitter(chunk_size=512)
+nodes = node_parser.get_nodes_from_documents(documents)
+
+qa_dataset = generate_question_context_pairs(
+    nodes, llm=llm, num_questions_per_chunk=2
+)
+# from llama_index.core.schema import TextNode
+# nodes = [TextNode(chunk) for chunk in chunks]
+
+queries = qa_dataset.queries.values()
+print(list(queries)[2])
