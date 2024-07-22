06573a9c47b74c50b167e5ca803eefb40243fb91
Kelton8Z <zhuoran2@andrew.cmu.edu>
2024-07-22 14:53:23 +0800

    test GPTs table retrieval

--- test/testTableRetrieval.py
+++ test/testTableRetrieval.py
@@ -3,15 +3,10 @@ from openai import OpenAI
 import os
 import streamlit as st
 
-os.environ["OPENAI_API_BASE"] = ""
+# os.environ["OPENAI_API_BASE"] = ""
 os.environ["OPENAI_API_KEY"] = st.secrets.openai_key
-openai_client = OpenAI()
-openai_assistant = openai_client.beta.assistants.create(
-  name="Financial Analyst Assistant",
-  instructions="You are an expert ai infra analyst. Use your knowledge base to answer questions about ai model/hardware performance.",
-  model="gpt-4o",
-  tools=[{"type": "code_interpreter"},{"type": "file_search"}]
-)
+openai_client = OpenAI(api_key=st.secrets.openai_key)
+
 
 import anthropic
 
@@ -37,7 +32,7 @@ def test_function_performance(model_type, model, input_len, output_len, throughp
     ch_question = f'{model}模型输入长度为{input_len}输出长度为{output_len}时吞吐量(request/s)，吞吐量(tokens/s)，batch是1、8、64的平均延迟分别是多少? 只返回最终答案数字[Throughput（requests/s), Throughput（tokens/s), Avg latency（Batch=1）, Avg latency（Batch=8）, Avg latency（Batch=64）]'
     en_question = f'With input leng {input_len}, output length {output_len}, what are the Throughput（requests/s), Throughput（tokens/s), Avg latency（Batch=1）, Avg latency（Batch=8）, Avg latency（Batch=64）for {model}? Only return the numbers'
 
-    if model_type=="gpt4o":
+    if model_type.startswith("gpt"):
         # Create a thread and attach the file to the message
         thread = openai_client.beta.threads.create(
           messages=[
@@ -64,41 +59,55 @@ def test_function_performance(model_type, model, input_len, output_len, throughp
         message_content = messages[0].content[0].text
         msg = message_content.value
     elif model_type=="claude3.5sonnet":
-        message = anthropic_client.messages.create(
-            model="claude-3-sonnet-20240229",
-            max_tokens=1024,
-            messages=[
-                {"role": "user", "content": md_table+"\n"+en_question}
-            ]
-        )
-        msg = message.content[0].text
+        try:
+            message = anthropic_client.messages.create(
+                model="claude-3-sonnet-20240229",
+                max_tokens=1024,
+                messages=[
+                    {"role": "user", "content": md_table+"\n"+en_question}
+                ]
+            )
+            msg = message.content[0].text
+        except Exception as e:
+            print(f"An error occurred: {e}")
+    else:
+        print("model not found")
+        return None
         
     cur_hit = sum([str(throughput_requests) in msg, str(throughput_tokens) in msg, str(avg_latency_batch1) in msg, str(avg_latency_batch8) in msg, str(avg_latency_batch64) in msg])      
     return cur_hit, 5-cur_hit
 
 def main(file_type, model_type):
   # Ready the files for upload to LLM 
+  file_path = excel_file_path
   df = pd.read_excel(excel_file_path)
   md_table = ""
   message_file = None
   
   if file_type=="md":
-      md_file_path = excel_file_path.split('.xlsx')[0]+'.md'
+      file_path = excel_file_path.split('.xlsx')[0]+'.md'
       md_table = df.to_markdown(tablefmt="grid")
-      with open(md_file_path, 'w') as file:
+      with open(file_path, 'w') as file:
           file.write(md_table)
   elif file_type=="csv":
-      csv_file_path = excel_file_path.split('.xlsx')[0]+'.csv'
+      file_path = excel_file_path.split('.xlsx')[0]+'.csv'
       csv_table = df.to_csv()
-      with open(csv_file_path, 'w') as file:
+      with open(file_path, 'w') as file:
           file.write(csv_table)
     
-  if model_type=="gpt4o":
-      # Create a vector store caled "Financial Statements"
+  if model_type.startswith("gpt"):
+      global openai_assistant
+      openai_assistant = openai_client.beta.assistants.create(
+        name=" Analyst Assistant",
+        instructions="You are an expert ai infra analyst. Use your knowledge base to answer questions about ai model/hardware performance.",
+        model=model_type,
+        tools=[{"type": "code_interpreter"},{"type": "file_search"}]
+      )
+      # Create a vector store
       vector_store = openai_client.beta.vector_stores.create(name="Model Performances")
       # Upload the user provided file to OpenAI
       message_file = openai_client.files.create(
-        file=open(md_file_path, "rb"), purpose="assistants"
+        file=open(file_path, "rb"), purpose="assistants"
       )
 
       print(f'provided {message_file.id} to openai')
